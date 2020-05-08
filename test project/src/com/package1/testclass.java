b60d157250f6e41ae62a8b836f885ff5357dee6e
SESA570968 <SESA570968@WTIN05204099L.apa.gad.schneider-electric.com>
2020-05-08 12:32:50 +0530

    commit 2

--- test project/src/com/package1/testclass.java
+++ test project/src/com/package1/testclass.java
@@ -4,7 +4,8 @@ public class testclass {
 public static void main(String args[])
 {
 	System.out.println("hello");
+	System.out.println("hello world");
 	System.out.println("hello sanam");
-}
+	}
 
 }
